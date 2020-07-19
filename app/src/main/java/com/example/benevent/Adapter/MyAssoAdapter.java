bc659f7efcfee8da12fff9da43cc80dab1428458
coren <corentin.delage@hotmail.fr>
2020-07-19 22:01:42 +0200

    cc

--- app/src/main/java/com/example/benevent/Adapter/MyAssoAdapter.java
+++ app/src/main/java/com/example/benevent/Adapter/MyAssoAdapter.java
@@ -3,9 +3,7 @@ package com.example.benevent.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
--- app/src/main/java/com/example/benevent/Adapter/MyCategoryAdapter.java
+++ app/src/main/java/com/example/benevent/Adapter/MyCategoryAdapter.java
@@ -3,7 +3,6 @@ package com.example.benevent.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
